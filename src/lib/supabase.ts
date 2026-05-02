0c78a224d029e5f7c425927d26f8cc96b0f63d75
Kungjakkid <148857819+Kungjakkid@users.noreply.github.com>
2026-05-02 15:08:55 +0700

    embed supabase credentials + update gitignore

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+const supabaseUrl = 'https://uexsgzexqoqpluiominb.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVleHNnemV4cW9xcGx1aW9taW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNTYwOTcsImV4cCI6MjA5MjkzMjA5N30.DltSnjAdPcEMB9VXQmwS0nFh7YifU-IX8jjY3yqAOW4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
